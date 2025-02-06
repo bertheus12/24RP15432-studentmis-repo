3fb69ae8ee3e3bcb4bc5e573f5a3ed4c6a31de77
bertheus12 <mukaberthe12@gmail.com>
2025-02-06 11:57:42 +0200

    index page is created

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>student MIS</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <center>
+    <div class="container">
+        <h1>Welcome to Student MIS</h1>
+        
+        <a href="registeration.php" class="btn">Register Here</a>
+    </div></center>
+</body>
+</html>
